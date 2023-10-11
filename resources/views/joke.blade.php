2afe2f358ad4e22091129be58499233d9f1d9ccd
CarloCassini <c.cassini87@gmail.com>
2023-10-11 17:35:04 +0200

    try

--- resources/views/joke.blade.php
+++ resources/views/joke.blade.php
@@ -4,13 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"
-        defer>
-    </script>
+    <title>Document< /title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+                integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+                crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+                crossorigin="anonymous" defer>
+            </script>
 </head>
 
 <body>
@@ -28,12 +29,12 @@
     </nav>
     <div class="container my-5">
         <div>
-            <h3 class="mt-3"> dillo alla</h3>
+            <h3 class="mt-3"> dillo a</h3>
 
             <h2 class="text-danger ">
                 {{$lista[$randomInd1]}}
             </h2>
-            <h3 class="mt-3"> dillo all'</h3>
+            <h3 class="mt-3"> dillo a</h3>
 
             <h2 class="text-danger ">
                 {{$lista[$randomInd2]}}
