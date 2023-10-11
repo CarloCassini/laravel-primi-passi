5f8a6ceb179f3045dc05c2be8aad906577f1dd68
CarloCassini <c.cassini87@gmail.com>
2023-10-11 17:33:56 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,15 +10,15 @@
 <body>
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid py-5"> <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button?>div
-                class="collapse navbar-collapse" id="navbarNavAltMarkup"> <div class="navbar-nav w-100 px-5"> <a
-                        class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
-                    <a class=" ms-auto nav-link active" aria-current="page" href="{{ route('joke') }}">non ti
-                        piace?</a>
-                </div>
-        </div>
+        <div class="container-fluid">
+
+            <div class="collapse navbar-collapse" id="navbarText">
+
+                <a class="nav-link active mx-3" aria-current="page" href="{{ route('home') }}">Home</a>
+                <a class="nav-link active mx-3" aria-current="page" href="{{ route('joke') }}">non mi piace</a>
+
+
+            </div>
         </div>
     </nav>
 
